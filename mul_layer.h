ad584b6816e519bf625a58df0f75a942edbefd92
Ryansanity <33254134+Ryansanity@users.noreply.github.com>
2021-05-11 22:52:03 +0800

    Add files via upload

---
+++ mul_layer.h
@@ -0,0 +1,28 @@
+#ifndef CAFFE_MUL_LAYER_HPP_
+#define CAFFE MUL LAYER HPP_
+#include <vector>
+#include"caffe/blob.hpp"
+#include"caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h" 
+namespace caffe{
+	template<typename Dtype>
+	class MulLayer:public Layer<Dtype>{
+	public:
+		explicit MulLayer(const LayerParameter& param):Layer<Dtype>(param){}
+		virtual void LayerSetUp(const vector<Blob<Dtype>*>&bottom, const vector<Blob<Dtype>*>&top);
+		virtual void Reshape(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>&top):
+		virtual inline const char* type() const { return "Mul";}
+		virtual inline int ExactNumBottomBlobs() const {return 1;} 
+		virtual inline int ExactNumTopBlobs() const{return1;} 
+	protected:
+		virtual void Forward_cpu(const vector<Blob<Dtype>*>&bottom, const vector<Blob<Dtype>*>&top);
+		virtual void Backward_cpu(const vector<Blob<Dtype>*>&top,
+		const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+		int W_;
+		int H_;
+		int M_;
+		Blob<Dtype>bias_multiplier_;
+	}
+}
+
+#endif
